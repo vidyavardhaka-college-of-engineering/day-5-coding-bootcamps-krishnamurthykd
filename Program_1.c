739545bacfe621da1d0cf1691990c7c712c2602e
Krishnamurthy D <krishnamurthykdb@gmail.com>
2021-02-19 11:16:15 +0000

    Program_1.c

--- Program_1.c
+++ Program_1.c
@@ -4,10 +4,17 @@
 
 int main()
 {
-	char str[10];
-	scanf("%s", str);
-
-	//Write your code here
-
+	char str[20];
+	int i,count=0;
+  scanf("%s", str);
+  for(i=0;str[i]!='\0';i++)
+  {if(str[i]=='a'||
+   str[i]=='e'||
+   str[i]=='i'||
+   str[i]=='o'||
+   str[i]=='u')
+{count++;}
+  }
+printf("%d",count);
 	return 0;
 }
